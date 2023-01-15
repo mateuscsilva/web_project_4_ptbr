609ce9b4891fe392f976a6c861a90f495d65e4aa
mateuscsilva <mateuscsilva.1@gmail.com>
2023-01-15 20:50:48 -0300

    feature: add responsivite to popup form

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,42 @@
 let likeButtons = document.querySelectorAll('.cards__like-button');
-
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let popupWindow = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
 
 function clickLikeButton(){
   this.classList.add("cards__like-button-clicked");
+}
+
+function openPopupWindow(){
+  popupWindow.classList.add('popup__opened');
+  let nameInput = document.querySelector('.popup__name');  
+  let jobInput = document.querySelector('.popup__about');
+  let name = document.querySelector('.profile__name');
+  let job = document.querySelector('.profile__work');
+  nameInput.value = name.textContent;
+  jobInput.value = job.textContent;
+}
+
+function closePopupWindow(){
+  popupWindow.classList.remove('popup__opened');
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = document.querySelector('.popup__name');  
+  let jobInput = document.querySelector('.popup__about');
+  let name = document.querySelector('.profile__name');
+  let job = document.querySelector('.profile__work');
+  name.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  popupWindow.classList.remove('popup__opened');
 }
 
 for(var i = 0; i < likeButtons.length; i++){
   likeButtons[i].addEventListener("click", clickLikeButton);
 }
 
+editButton.addEventListener("click", openPopupWindow);
+closeButton.addEventListener("click", closePopupWindow);
+formElement.addEventListener("submit", handleProfileFormSubmit); 
